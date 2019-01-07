62cfcef5c1fd52787bef738ad2ce450c324fb28e
rwburden <rwburden@yahoo.com>
2019-01-07 09:29:55 -0500

    First draft of script to make old EIR pages responsive.  processNRpage.js belongs in /r/js on the local mirror of the EIR website.  A local webserver must be running with document root at the root of the local mirror of the EIR website in order to successfully run this script.

---
+++ makeEIRpagesResponsive.pl
@@ -0,0 +1,271 @@
+=head1 SYNOPSIS
+
+perl makeEIRpagesResponsive.pl {-|<config pathname>} [options] <pathname>
+
+=head1 OPTIONS
+
+Each option must be preceded by a double hyphen '--' to indicate that it is an option, except:
+options with single character names may be preceded by a single hyphen '-'.
+
+Alternative names for an option are separated by '|'.
+
+Options whose name(s) are followed by '=' require an argument.  The letter following the '=' specifies the argument type (s for string). If the argument type is folllowed by '%', the option  may be used multiple times in the same command line with an argument for each instance.
+
+ appendBrowserScript|abs
+
+appendBrowserScript|abs causes the browser script to be appended instead of overwritten. Regardless of options, this script generates a browser script that can be used to invoke a browser to download  all the non-responsive EIR pages that are linked from the EIR home page, insert JavaScript in those pages, execute the JavaScript, then write the result.  The browser script is written to the value of 'browserScriptPath' in the config file.  Each line of the browser script will contain bp bo URL > lpp, where 'bp' is the value of 'browserPath' in the config file, 'bo' the value of 'browserOptions', 'URL' the absolute URL of the page on the site, and 'lpp' is the local page path, the absolute path of the location in the local file system corresponding to the URL to which the new responsive page will be uploaded. The 'docroot' attribute of the 'domain' tag in the config file is prepended to the path portion of the URL to calculate the lpp.  If the config. file lacks 'browserOptions', '--headless --dump-dom', the options appropriate for Chrome and other Chromium-based browsers such as Yandex, are inserted by default.
+
+=cut
+
+use HTML::TreeBuilder;
+use HTML::Element;
+use File::Spec;
+use File::Find;
+use File::Path qw(make_path remove_tree);
+use Fcntl ':mode';
+use File::Copy;
+use HTML::Entities;
+use URI;
+use WWW::Mechanize;
+use Getopt::Long qw(:config no_ignore_case);
+use Pod::Usage;
+use Cwd;
+
+$HTML::Tagset::isKnown{"domain"} = 1;
+$HTML::Tagset::isHeadOrBodyElement{"domain"} = 1;
+
+%empty = (); #use as 3rd parameter for HTML::Element->as_HTML() to specify that the HTML generated shall close all open tags
+
+my $help = 0;
+my $appendBrowserScript = 0;
+
+GetOptions ('help|?' => \$help,'appendBrowserScript|abs' => \$appendBrowserScript);
+pod2usage(1) if $help;
+
+my $program_basename = $0;
+$program_basename =~ s/\.[^\.]*$//;
+close STDERR;
+open (STDERR, ">$program_basename"."_err.txt") || die "can't open $program_basename"."_err.txt for writing: $!";
+
+print "\nError messages will written to $program_basename"."_err.txt in the current working directory\n\n";
+
+if (@ARGV < 2) {usage()}
+#debug output is off by default.  It is turned on by using 'debug' as the first command-line argument OR by defining debug with any value in the configuration file.
+my $debug = 0;
+if (lc($ARGV[0]) eq 'debug')
+{
+    $debug = 1;
+    shift @ARGV
+}
+if (@ARGV < 2) {usage()}
+
+#process the config file selector
+my $configPath = $ARGV[0];
+
+if ($configPath eq '-') {$configPath = "$program_basename.xml"}
+else {$configPath = "$program_basename-$configPath.xml"}
+
+
+
+my $domain;
+my %options = ();
+get_config ($configPath, \$domain, \%options);
+
+#debug output is off by default.  It is turned on by using 'debug' as the first command-line argument OR by defining debug with any value in the configuration file.
+if (defined $options{'debug'}){$debug = 1}
+
+my $docRoot = $domain->attr('docroot');
+my $domainName = $domain->attr('name');
+
+my $bsp = $options{'browserScriptPath'};
+if (not defined $bsp) {$bsp = 'epub2htmlRunJS.bat'}
+my $browserPath = $options{'browserPath'};
+my $browserOptions = $options{'browserOptions'};
+if (not defined $browserOptions) {$browserOptions = '--headless --dump-dom'}
+if (not defined $browserPath) {$browserPath = '%BROWSER_PATH%'}
+my $responsivePagesPathPrefix = $options{'responsivePagesPathPrefix'};
+my $nullDevice = $options{'nullDevice'};
+my $localHostURL = $options{'localHostURL'};
+my $articleTemplatePath = $options{'articleTemplatePath'};
+if (not defined $responsivePagesPathPrefix) {$responsivePagesPathPrefix = '\r'}
+if (not defined $nullDevice) {$nullDevice = 'nul'}
+if (not defined $localHostURL) {$localHostURL = 'http://localhost:8000'}
+if (not defined $articleTemplatePath) {$articleTemplatePath = 'process_nr_article_template.html'}
+
+
+if ($appendBrowserScript)
+{
+    open (BROWSERSCRIPT, ">>$bsp") || die "can't open $bsp for appending: $!";
+}
+else
+{
+    open (BROWSERSCRIPT, "+>$bsp") || die "can't open $bsp for writing: $!";
+}
+
+open(ARTICLETEMPLATE, $articleTemplatePath) || die "can't open $articleTemplatePath for reading: $!";
+#my $articletemplate = "";
+#while (<ARTICLETEMPLATE>) {$articletemplate .= $_}
+#close ARTICLETEMPLATE;
+
+my $attree = HTML::TreeBuilder->new;
+$attree->parse_file(\*ARTICLETEMPLATE);
+
+#process the input file/directory/glob pattern.
+my $inpath = "";
+$inpath = $ARGV[1];
+#Extract filenames from $inpath
+
+$inpath =~ s%/%\\%g;
+
+$inpath = File::Spec->join($docRoot,$inpath);
+
+@inpath = File::Spec->splitpath($inpath);
+@infiles = ();
+
+my $is_directory = 0;
+my $mode = undef;
+$mode = (stat($inpath))[2];
+if (defined $mode)
+{$is_directory = S_ISDIR($mode)}
+
+if ($is_directory)
+{
+    find (\&wanted, $inpath);
+}
+elsif ($inpath[2] =~ /[\?\*]/)
+{
+    print "globpath: $inpath\n";
+    @infiles = glob ($inpath);
+}
+
+else
+{@infiles = ($inpath)}
+
+
+foreach $infilepath (@infiles)
+{
+    my @infile = File::Spec->splitpath($infilepath);
+    my $infileDir = File::Spec->join($infile[0],$infile[1]);
+    my $siteRelInfileDir = substr($infileDir,length($docRoot));
+    my $outfiledir = $docRoot.$responsivePagesPathPrefix.$siteRelInfileDir;
+    my $err = undef;
+    make_path($outfiledir, {error => \$err});
+    if (@$err) {
+	for my $diag (@$err) {
+	    my ($dir, $message) = %$diag;
+	    if ($dir eq '') {
+		print STDERR "general error: $message\n";
+	    }
+	    else {
+		print STDERR "problem creating $dir: $message\n";
+	    }
+	}
+	next;
+    }
+    
+    my $outfile = File::Spec->join($outfiledir,$infile[2]);
+    if (! open(INFILE, $infilepath)){print STDERR "can't open $infilepath for reading: $!"; next}
+  #  if (! open (OUTFILE,"+>$outfile")){print STDERR "can't open $outfile for writing: $!"; next}
+    if ($debug)
+    {
+	open(DEBUG,">$outfile.debug.html") || die "can't open $outfile.debug.html for writing: $!"; 
+    }
+    else
+    {
+	open(DEBUG, ">$nullDevice") || die "can't open $nullDevice for writing: $!"; 
+    }
+    my $tree = HTML::TreeBuilder->new();
+    $tree->store_comments(1);
+    $tree->parse_file(\*INFILE);
+    my $dirURL = $outfiledir;
+    $dirURL = $localHostURL . substr($dirURL,length($docRoot));
+    $dirURL =~ s%\\%/%g;
+    my $savedDir = getcwd();
+    #open temporary file for writing
+    open (TEMP,"+>$outfiledir\\temp.html") || die "can't open $outfiledir\\temp.html for writing: $!";
+    #insert script tags
+  #  my $jquery = HTML::Element->new('script','type'=>'text/javascript','src'=>$jqueryURL);
+   # my $script = HTML::Element->new('script','type'=>'text/javascript','src'=>$processNRpageURL);   my $initscript = HTML::Element->new('script','type'=>'text/javascript');
+    #$initscript->push_content('$(window).load(processNRpage);');
+  #  my $head = $attree->find_by_tag_name('head');
+   # $head->push_content($jquery,$script,$initscript);
+	
+    #write $tree to a temporary HTML file
+ #   print TEMP $tree->as_HTML("","\t",\%empty);
+    #append content of article template
+ #   print TEMP $articletemplate;
+
+    my @acontent = $tree->find_by_tag_name('body')->content_list;
+    my @ahead = $tree->find_by_tag_name('head')->content_list;
+    my $acontentdiv = HTML::Element->new('div','id','old_article_content');
+    $acontentdiv->push_content(@acontent);
+    my $atbody = $attree->find_by_tag_name('body');
+    $atbody->push_content($acontentdiv);
+    my $athead = $attree->find_by_tag_name('head');
+    $athead->push_content(@ahead);
+    print TEMP $attree->as_HTML("","\t",\%empty);
+    close TEMP;
+    #use a browser to execute the JavaScript on the temporary HTML file and save to $outfile
+    chdir $outfiledir;
+    my $cmd = '"'.$browserPath.'" '.$browserOptions.' "'.$dirURL.'/temp.html" > "'.$outfile.'"';
+    system($cmd);
+    chdir $savedDir;
+}
+
+sub wanted
+{
+    if (not -f $_) {next}
+    if ($_ !~ /\.(html?)$/i) {next}
+    my $path = $File::Find::name;
+    $path =~ s%/%\\%g;
+    push (@infiles,$path);
+}
+
+sub get_config
+{
+	my ($configPath, $domainRef, $optionsRef) = @_;
+
+	open (CONFIG, "$configPath") || die "can't open $configPath for reading: $!";
+
+	my $tree = HTML::TreeBuilder->new();
+	$tree->store_comments(1); #to exclude commented-out configuration code from interpretation
+	$tree->parse_file(\*CONFIG);
+	$$domainRef = $tree->find_by_tag_name ("domain");
+	my @options = $tree->find_by_tag_name ("option");
+	foreach $option (@options)
+	{
+		my $key = $option->attr('id');
+		my $value = $option->attr('value');
+		if (defined $value)
+		{
+			$$optionsRef{$key} = $value;
+		}
+		else
+		{
+			$$optionsRef{$key} = '';
+		}
+	}
+}
+
+
+sub usage
+{
+    my $dcf = $program_basename.'.xml';
+    my $options_doc = "";
+	
+    print <<EOF;
+perl $0 [debug] {-|<config pathname>} [options] <pathname>
+<config pathname> is the pathname of the configuration file, relative to $0
+- uses the default configuration file, $dcf
+<pathname> is the pathname of the input file, relative to the website
+document root as specified in the configuration file, however:
+It should begin with a forward slash, use only forward slashes, and
+should be quoted if it contains spaces.  The spaces and other non-web-safe
+characters should not be converted to web-safe entities like '%20'
+
+For a list of options, use perl $0 - -help
+
+EOF
+
+exit
+}
---
+++ processNRpage.js
@@ -0,0 +1,200 @@
+// JavaScript Document
+jQuery.expr[':'].regex = function(elem, index, match) {
+    var matchParams = match[3].split(','),
+    validLabels = /^(data|css):/,
+    attr = {
+	method: matchParams[0].match(validLabels) ? 
+	matchParams[0].split(':')[0] : 'attr',
+	property: matchParams.shift().replace(validLabels,'')
+    },
+    regexFlags = 'ig',
+    regex = new RegExp(matchParams.join('').replace(/^\s+|\s+$/g,''), regexFlags);
+    return regex.test(jQuery(elem)[attr.method](attr.property));
+};
+
+jQuery.fn.outerHTML = function(s) {
+    if (s) {
+        return this.before(s).remove();
+    } else {
+        var doc = this[0] ? this[0].ownerDocument : document;
+        return jQuery('<div>', doc).append(this.eq(0).clone()).html();
+    }
+};
+
+(function($) {
+    $.fn.closestPrior = function(selector) {
+        selector = selector.replace(/^\s+|\s+$/g, "");
+	console.log("selector: '"+selector+"'");
+        var combinator = selector.search(/[ +~>]|$/);
+	console.log("combinator: '"+combinator+"'");
+        var parent = selector.substr(0, combinator);
+		
+	console.log("parent: '"+parent+"'");
+        var children = selector.substr(combinator);
+	console.log("children: '"+children+"'");
+        var el = this;
+	console.log("el: " + el.outerHTML());
+        var match = $();
+        while (el.length && !match.length) {
+	    var savedel = el;
+	    console.log("el: " + el.outerHTML());
+	    console.log("el.prev(): " + el.prev().outerHTML());
+            el = el.prev();
+	    console.log("el: " + el.outerHTML());
+            if (!el.length) {
+                var par = savedel.parent();
+                // Don't use the parent - you've already checked all of the previous 
+                // elements in this parent, move to its previous sibling, if any.
+                while (par.length && !par.prev().length) {
+                    par = par.parent();
+                }
+                el = par.prev();
+                if (!el.length) {
+                    break;
+                }
+            }
+	    console.log("comparing " + el.outerHTML() + " with " + parent);
+	    console.log ("el.is(parent) is " + el.is(parent));
+	    console.log ("el.find(children).length is " + el.find(children).length);
+	    console.log ("el.find(selector).length is " + el.find(selector).length);
+            if (el.is(parent) && el.find(children).length) {
+		console.log("getting last child of " + el.outerHTML());
+                match = el.find(children).last();
+		console.log ("match found for " + this.outerHTML() + ": " + match.outerHTML());
+            }
+            else if (el.find(selector).length) {
+		console.log("selecting last of '" + selector + "' within " + el.outerHTML() + "(" + el.find(selector).length + " items)");
+
+                match = el.find(selector).last();
+		console.log ("match found for " + this.outerHTML() + ": " + match.outerHTML());
+            }
+	    else if (el.is(selector))
+		{
+		    match = el;	
+		    console.log ("match found for " + this.outerHTML() + ": " + match.outerHTML());
+		}
+        }
+        return match;
+    }
+})(jQuery);
+
+
+(function($) {
+    $.fn.closestNext = function(selector) {
+        selector = selector.replace(/^\s+|\s+$/g, "");
+	console.log("selector: '"+selector+"'");
+        var combinator = selector.search(/[ +~>]|$/);
+	console.log("combinator: '"+combinator+"'");
+        var parent = selector.substr(0, combinator);
+		
+	console.log("parent: '"+parent+"'");
+        var children = selector.substr(combinator);
+	console.log("children: '"+children+"'");
+        var el = this;
+	console.log(" el: " + el.outerHTML());
+        var match = $();
+        while (el.length && !match.length) {
+	    var savedel = el;
+            el = el.next();
+	    console.log(" el: " + el.outerHTML());
+            if (!el.length) {
+                var par = savedel.parent();
+                // Don't use the parent - you've already checked all of the previous 
+                // elements in this parent, move to its previous sibling, if any.
+                while (par.length && !par.next().length) {
+                    par = par.parent();
+                }
+                el = par.next();
+                if (!el.length) {
+                    break;
+                }
+            }
+	    console.log("comparing " + el.outerHTML() + " with " + parent);
+	    console.log ("el.is(parent) is " + el.is(parent));
+	    console.log ("el.find(children).length is " + el.find(children).length);
+	    console.log ("el.find(selector).length is " + el.find(selector).length);
+            if (el.is(parent) && el.find(children).length) {
+		console.log("getting first child of " + el.outerHTML());
+                match = el.find(children).first();
+		console.log ("match found for " + this.outerHTML() + ": " + match.outerHTML());
+            }
+            else if (el.find(selector).length) {
+		console.log("selecting first of '" + selector + "' within " + el.outerHTML() + "(" + el.find(selector).length + " items)");
+
+                match = el.find(selector).first();
+		console.log ("match found for " + this.outerHTML() + ": " + match.outerHTML());
+            }
+	    else if (el.is(selector))
+		{
+		    match = el;	
+		    console.log ("match found for " + this.outerHTML() + ": " + match.outerHTML());
+		}
+        }
+        return match;
+    }
+})(jQuery);
+
+function testClosestPrior()
+{
+    var mycapt = $('#testcapt');
+    console.log (mycapt.closestPrior('p.text').outerHTML());
+};
+
+// console.log = function() {};
+
+function getTextWidth(textobj)
+{
+var	o = $('<div></div>').text(textobj.text()).css({'position': 'absolute', 'float': 'left', 'white-space': 'nowrap', 'visibility': 'hidden', 'font-family':textobj.css('font-family'),'font-weight':textobj.css('font-weight'),'font-size':textobj.css('font-size'),'font-style':textobj.css('font-style')}).appendTo($('body')),
+      w = o.width();
+
+  o.remove();
+
+  return w;
+}
+
+function greatestTextWidth(objs)
+{
+	var greatest = 0;
+	console.log("objs.length: " + objs.length);
+	for (var i=0;i<objs.length;i++)
+	{
+		var w = getTextWidth(objs.eq(i));
+		if (w > greatest) {greatest = w;}
+	}
+	return greatest;
+}
+
+function checkTextWidth (textobj,maxTextWidth,imgwrapper,imgwrapper2)
+{
+if (imgwrapper.attr('style') === undefined)
+{
+	console.log('imgwrapper has no style attribute');
+	return imgwrapper;
+}
+var tw = getTextWidth(textobj);
+console.log('text: ' + textobj.text() + ' text width: ' + tw);
+if (tw > maxTextWidth) 
+{
+	imgwrapper2.append(imgwrapper.clone());
+	console.log("imgwrapper2: " + imgwrapper2.outerHTML());
+	imgwrapper.replaceWith(imgwrapper2);
+	return imgwrapper2;
+}
+return imgwrapper; 	
+}
+
+function getImageDimensions(imageNode) {
+  var source = imageNode.attr('src');
+	console.log("getImageDimensions: source: " + source); 
+  var imgClone = document.createElement("img");
+  imgClone.src = source;
+  return {width: imgClone.width, height: imgClone.height};
+}
+
+function processNRpage()
+{
+    var mycontent = $('body').contents();
+    $('<div id="convertedPage"></div>').appendTo('body');
+    mycontent.appendTo('#convertedPage');
+	$('script').remove();
+}
